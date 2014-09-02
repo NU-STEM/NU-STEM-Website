c4dffc0b74a008fa9add82b7df20547cbd9978f5
Ryan Brott <r.brott@comcast.net>
2014-09-01 17:13:52 -0700

    Initial Heroku Deploy

---
+++ build.js
@@ -0,0 +1,54 @@
+var fs = require("fs"),
+	sass = require("node-sass"),
+	compressor = require("node-minify"),
+	path = require("path"),
+	_ = require("lodash");
+var fontsDir = __dirname + "/fonts/",
+	imagesDir = __dirname + "/images/",
+	scssDir = __dirname + "/scss/",
+	cssDir = __dirname + "/css/",
+	jsDir = __dirname + "/js/",
+	files;
+files = fs.readdirSync(scssDir);
+for (var i = 0, len = files.length; i < len; i++) {
+	file = files[i]
+	ext = path.extname(file);
+	filename = path.basename(file, ext);
+	fs.writeFileSync(cssDir + filename + ".css", sass.renderSync({
+		data: fs.readFileSync(scssDir + file)
+	}));
+}
+files = fs.readdirSync(cssDir);
+files = _.filter(files, function(file) {
+	ext = path.extname(file);
+	filename = path.basename(file, ext);
+	return path.extname(filename) != ".min";
+});
+files = _.map(files, function(file) {
+	return cssDir + file;
+});
+new compressor.minify({
+	type: "clean-css",
+	fileIn: files,
+	fileOut: cssDir + "stylesheet.min.css",
+	callback: function(err, min) {
+		if (err) console.warn(err);
+	}
+});
+files = fs.readdirSync(jsDir);
+files = _.filter(files, function(file) {
+	ext = path.extname(file);
+	filename = path.basename(file, ext);
+	return path.extname(filename) != ".min";
+});
+files = _.map(files, function(file) {
+	return jsDir + file;
+});
+new compressor.minify({
+	type: "gcc",
+	fileIn: files,
+	fileOut: jsDir + "script.min.js",
+	callback: function(err, min) {
+		if (err) console.warn(err);
+	}
+});
---
+++ server.js
@@ -0,0 +1,57 @@
+var express = require("express"),
+	jade = require("jade"),
+	sass = require("node-sass");
+	path = require("path"),
+	fs = require("fs"),
+	GitHubApi = require("github"),
+	_ = require("lodash")
+	app = express(),
+	github = new GitHubApi({
+		version: "3.0.0",
+		debug: true,
+		protocol: "https",
+		host: "api.github.com"
+	});
+app.locals = {
+	"github": {}
+};
+github.orgs.get({
+	"org": "NU-STEM"
+}, function(err, data) {
+	if (err) console.warn(err);
+	else {
+		app.locals.github.org = data;
+	}
+});
+github.repos.getFromOrg({
+	"org": "NU-STEM"
+}, function(err, data) {
+	if (err) console.warn(err);
+	else {
+		// Sort data by date and limit to 5
+		// Name in the name property
+		// Date updated at in the updated_at property
+		app.locals.github.repos = data;
+	}
+});
+var fontsDir = __dirname + "/fonts/",
+	imagesDir = __dirname + "/images/",
+	scssDir = __dirname + "/scss/",
+	cssDir = __dirname + "/css/",
+	jsDir = __dirname + "/js/";
+app.set("views", __dirname + "/jade");
+app.set("view engine", "jade");
+app.engine("jade", jade.__express);
+app.use("/css", express.static(cssDir));
+app.use("/js", express.static(jsDir));
+app.use("/fonts", express.static(fontsDir));
+app.use("/images", express.static(imagesDir));
+app.use("/scss", function(req, res) {
+	res.end(sass.renderSync({
+		data: fs.readFileSync(scssDir + path.basename(req.path))
+	}));
+});
+app.get("/", function(req, res) {
+	res.render("index");
+});
+app.listen(80);
